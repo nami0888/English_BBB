411f76c1572d5ae3b641603d3ea5728b61d60fb8
nami <kgnogn0888@gmail.com>
2022-01-17 08:25:40 +0900

    自宅修正0116

--- main.js
+++ main.js
@@ -13,11 +13,13 @@ $(function(){
     hamburger();
   });
 
-  /*=================================================
+
+
+
+/*=================================================
   Inview（画面に表示されたタイミングで処理を実行）
   ===================================================*/
-  // BBBが選ばれる理由（スライド左）
-  $('.inview-slide-left').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+$('.inview-slide-left').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
     if(isInView){
       // 要素が表示されたらslide-leftクラスを追加
       $(this).stop().addClass('slide-left');
@@ -39,21 +41,22 @@ $(function(){
   });
 });
 
-/*=================================================
-ハンバーガ―メニュー共通処理
-===================================================*/
-// ハンバーガーメニューをクリックした時とメニュー内のリンクをクリックした時の
-// 処理が同じなので処理を共通化する
+
+
+
+
+
+
+
 function hamburger() {
-  // toggleClassを使用することで、hamburgerクラスにactiveクラスが存在する場合は削除、
-  // 存在しない場合を追加する処理を自動で行ってくれる
+
   $('.hamburger').toggleClass('active');
 
   if ($('.hamburger').hasClass('active')) {
-    // hamburgerクラスにactiveクラスが存在する場合は、naviにもactiveクラスを追加する
+
     $('#navi').addClass('active');
   } else {
-    // hamburgerクラスにactiveクラスが存在しない場合は、naviからactiveクラスを削除する
+
     $('#navi').removeClass('active');
   }
 }
